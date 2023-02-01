08ede65be6fc2a77921effcc61dd4fd3bbaf93d6
JonathanYin <joyin@ucsd.edu>
2023-01-31 16:57:14 -0800

    Add link to sustainability page

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,12 @@
 const menu = document.getElementById("menu");
 
 Array.from(document.getElementsByClassName("menu-item"))
-  .forEach((item, index) => {
-    item.onmouseover = () => {
-      menu.dataset.activeIndex = index;
-    }
-  });
+.forEach((item, index) => {
+  item.onmouseover = () => {
+    menu.dataset.activeIndex = index;
+  }
+});
+
+document.querySelector('#learn').addEventListener('click', function(e) {
+  window.location.href = 'sustainability.html';
+}, false);
